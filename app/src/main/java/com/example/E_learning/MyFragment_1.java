c82ed3b81dcab247d53049f8ec67c755ed42dd3b
Nabil Madih <nabilmadih.a@gmail.com>
2019-10-28 05:17:09 +0800

    Uploading our project

---
+++ app/src/main/java/com/example/E_learning/MyFragment_1.java
@@ -0,0 +1,27 @@
+package com.example.E_learning;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.elearning.R;
+
+public class MyFragment_1 extends Fragment {
+    private String content;
+
+    public MyFragment_1(String content){
+        this.content = content;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment,container,false);
+        TextView textContent =(TextView)view.findViewById(R.id.textContent);
+        textContent.setText(content);
+        return view;
+    }
+}
